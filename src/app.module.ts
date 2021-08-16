a4acb44ca3665b1b13d65dca08e6c9a92e80de17
MEBY98 <mebordony@gmail.com>
2021-08-16 11:24:35 -0400

    general configuration and schema.gql update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,21 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { FraseograficStudyModule } from './fraseograficStudy/fraseograficStudy.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { DescriptorModule } from './descriptor/descriptor.module';
-import { EntryModule } from './entry/entry.module';
-import { DictionaryModule } from './dictionary/dictionary.module';
-import { AlphabetModule } from './alphabet/alphabet.module';
+import { MONGO_OPTIONS, MONGO_URL } from '../configuration/mongo.config';
+import { MinioModule } from './minIO/minio.module';
+import { ClasificationModule } from './clasification/clasification.module';
+import { DescriptorTypeModule } from './descriptorType/descriptorType.module';
+import { ObservationModule } from './observation/observation.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/Dictionary', {
-      useFindAndModify: false,
-    }),
+    MongooseModule.forRoot(MONGO_URL, MONGO_OPTIONS),
     GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
     FraseograficStudyModule,
+    MinioModule,
+    ClasificationModule,
+    DescriptorTypeModule,
+    ObservationModule,
   ],
 })
 export class AppModule {}
