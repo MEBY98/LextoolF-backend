dcf1df11542574e1a4aaad61f5c7d8c12c8e48aa
MEBY98 <mebordony@gmail.com>
2021-08-27 07:04:24 -0400

    delete lemma sublemma UF author

--- src/lemma/lemma.resolver.ts
+++
@@ -1,7 +0,0 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
-import { LemmaService } from './lemma.service';
-
-@Resolver()
-export class LemmaResolver {
-  constructor(private readonly LemmaService: LemmaService) {}
-}
--- src/lemma/model/lemma.modelinterface.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface Lemma extends Document {
-  lemma: string;
-  clasification: string;
-  // descriptors: String[];
-}
