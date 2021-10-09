8cde106b8a6ba2baef259e8ae9fb8b5592b93ac4
MEBY98 <mebordony@gmail.com>
2021-10-09 13:17:26 -0400

    clean unUsedVariables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { FraseograficStudyModule } from './fraseograficStudy/fraseograficStudy.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MONGO_OPTIONS, MONGO_URL } from '../configuration/mongo.config';
 import { MinioModule } from './minIO/minio.module';
 import { DescriptorTypeModule } from './descriptorType/descriptorType.module';
 import { ObservationModule } from './observation/observation.module';
@@ -29,4 +28,4 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ObservationModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
